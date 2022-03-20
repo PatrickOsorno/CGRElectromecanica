5f4469ffa465be5ff684f819d785debc64478919
PatrickOsorno <patosorno01@gmail.com>
2022-03-20 03:10:02 -0600

    Mapa

--- js/mapa.js
+++ js/mapa.js
@@ -1,15 +1,29 @@
-// Initialize and add the map
-function initMap() {
-    // The location of Uluru
-    const uluru = { lat: 9.995354, lng: -84.252274 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: uluru,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-    });
-  }
+function iniciarMapa() {
+  var coord = { lat: 10.141323, lng: -84.194666 };
+  const directionsService = new google.maps.DirectionsService();
+  const directionsRenderer = new google.maps.DirectionsRenderer();
+  var map = new google.maps.Map(document.getElementById('mapa'), {
+    zoom: 15,
+    center: coord
+  });
+  var marker = new google.maps.Marker({
+    position: coord,
+    map: map
+  });
+
+  directionsRenderer.setMap(map);
+  calcularYMostarRuta(directionsService, directionsRenderer);
+}
+
+function calcularYMostarRuta(directionsService, directionsRenderer) {
+  directionsService
+    .route({
+      origin: { lat: 9.995415, lng: 9.995415 },
+      destination: { lat: 10.141323, lng: -84.194666 },
+      travelMode: 'DRIVING'
+    })
+    .then((response) => {
+      directionsRenderer.setDirections(response);
+    })
+    .catch((e) => window.alert("Directions request failed due to " + e));
+}
