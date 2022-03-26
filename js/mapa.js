fd67d4f22f39b6704652739b7d5a345f42bc4b5a
PatrickOsorno <patosorno01@gmail.com>
2022-03-26 00:11:35 -0600

    commit 26/3/22

--- js/mapa.js
+++ js/mapa.js
@@ -1,29 +1,38 @@
-function iniciarMapa() {
-  var coord = { lat: 10.141323, lng: -84.194666 };
-  const directionsService = new google.maps.DirectionsService();
+function initMap() {
   const directionsRenderer = new google.maps.DirectionsRenderer();
-  var map = new google.maps.Map(document.getElementById('mapa'), {
-    zoom: 15,
-    center: coord
+  const directionsService = new google.maps.DirectionsService();
+  var coord = { lat: 10.141323, lng: -84.194666 };
+  const map = new google.maps.Map(document.getElementById("mapa"), {
+    zoom: 5,
+    center: coord,
+    disableDefaultUI: true,
   });
-  var marker = new google.maps.Marker({
+
+  new google.maps.Marker({
     position: coord,
     map: map
   });
 
   directionsRenderer.setMap(map);
-  calcularYMostarRuta(directionsService, directionsRenderer);
+  calculateAndDisplayRoute(directionsService, directionsRenderer);
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    console.log(position.coords.latitude);
+  });
 }
 
-function calcularYMostarRuta(directionsService, directionsRenderer) {
+function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+  const start = { lat: 9.995379, lng: -84.252260 };
+  const end = { lat: 10.141323, lng: -84.194666 };
+
   directionsService
     .route({
-      origin: { lat: 9.995415, lng: 9.995415 },
-      destination: { lat: 10.141323, lng: -84.194666 },
-      travelMode: 'DRIVING'
+      origin: start,
+      destination: end,
+      travelMode: google.maps.TravelMode.DRIVING,
     })
     .then((response) => {
       directionsRenderer.setDirections(response);
     })
-    .catch((e) => window.alert("Directions request failed due to " + e));
+    .catch((e) => window.alert("Directions request failed due to " + status));
 }
