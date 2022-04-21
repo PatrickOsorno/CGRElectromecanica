7285e6dd1a60a4c8259502d875ad47d85f514b28
MeliArceG <melissapag9152002@gmial.com>
2022-04-20 19:56:23 -0600

    20-4

--- js/animaciones_galeria.js
+++ js/animaciones_galeria.js
@@ -1,13 +1,19 @@
-$(function(){
-    $(".galery").click(function(e){
-        var enlaceImagen = e.target.src;
-        var lightbox = '<div class= "lightbox">'+
-                            '<img src = "'+enlaceImagen+'"alt="">'+
-                            '<div class="btn-close">X</div>'+
-                        '</div>';
-        $("body").append(lightbox)
-        $(".btn-close").click(function(){
-            $(".lightbox").remove();
-        });
-    });
-});
+$(".galery").click(function(e){
+	var enlaceImagen = e.target.src;
+	var data = $(this).attr("data");
+	var lightbox = '<div class="ligthbox">'+
+						'<img src="'+enlaceImagen+'" alt="" id="zoom_mw" data-zoom-image="'+data+'">'+
+						'<div class="btn-close">X</div>'+
+					'</div>';
+
+	$("body").append(lightbox)
+	$("#zoom_mw").elevateZoom({
+		scrollZoom : true,
+		cursor: "crosshair",
+		zoomWindowOffetx: 15
+	});
+	$(".btn-close").click(function(){
+		$(".ligthbox").remove();
+	})
+
+})
